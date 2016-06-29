6e420ba3568077e55e92d876bdf8b39328b7c1b9
Thilo Schulz <thilo@tjps.eu>
2016-06-29 20:02:51 +0200

    - Fix differing prototypes in cencode.h and cdecode.h - Make string length argument constant for consistency in cencode.h

--- include/b64/cencode.h
+++ include/b64/cencode.h
@@ -22,9 +22,9 @@ typedef struct
 
 void base64_init_encodestate(base64_encodestate* state_in);
 
-char base64_encode_value(char value_in);
+char base64_encode_value(signed char value_in);
 
-int base64_encode_block(const char* plaintext_in, int length_in, char* code_out, base64_encodestate* state_in);
+int base64_encode_block(const char* plaintext_in, const int length_in, char* code_out, base64_encodestate* state_in);
 
 int base64_encode_blockend(char* code_out, base64_encodestate* state_in);
 
--- src/cencode.c
+++ src/cencode.c
@@ -23,7 +23,7 @@ char base64_encode_value(signed char value_in)
 	return encoding[(int)value_in];
 }
 
-int base64_encode_block(const char* plaintext_in, int length_in, char* code_out, base64_encodestate* state_in)
+int base64_encode_block(const char* plaintext_in, const int length_in, char* code_out, base64_encodestate* state_in)
 {
 	const char* plainchar = plaintext_in;
 	const char* const plaintextend = plaintext_in + length_in;
